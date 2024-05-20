c9540dd75d20ec9589718a5c8e458aafb5b6b0fc
MrDog210 <tusakf@gmail.com>
2024-05-20 14:16:43 +0200

    Update companyWallScraper.kt

--- src/main/kotlin/scraper/companyWall/companyWallScraper.kt
+++ src/main/kotlin/scraper/companyWall/companyWallScraper.kt
@@ -4,62 +4,11 @@ import it.skrape.core.htmlDocument
 import it.skrape.fetcher.BrowserFetcher
 import it.skrape.fetcher.extractIt
 import it.skrape.fetcher.skrape
-import it.skrape.selects.and
-import it.skrape.selects.html5.a
 import it.skrape.selects.html5.div
 import it.skrape.selects.html5.span
 import models.Company
 import scraper.emailRegex
-import scraper.getDocumentByUrl
 import scraper.ibanRegex
-
-/*fun scrapeCompanyWallPage(urlToScrape: String) {
-    val html = getDocumentByUrl(urlToScrape)
-    htmlDocument(html.html) {
-        div {
-            withAttribute = "itemtype" to "http://schema.org/Organization"
-            findFirst {
-                // name
-                println(text)
-            }
-        }
-        "small" {
-            withAttribute = "itemtype" to "http://schema.org/PostalAddress"
-            findFirst {
-                // address
-                println(text.replace(" ,", ","))
-            }
-        }
-        span {
-            withAttribute = "itemprop" to "vatID"
-            findFirst {
-                // davčna
-                println(text.replace(" ", ""))
-            }
-        }
-        div {
-            withClass = "contact-summary"
-            findFirst {
-                println(text)
-                val emailMatch = emailRegex.find(text)
-                if (emailMatch != null) {
-                    // email
-                    println(emailMatch.value)
-                }
-            }
-        }
-        div {
-            withClass = "bank-account-container"
-            findFirst {
-                val ibanMatch = ibanRegex.find(text)
-                if (ibanMatch != null) {
-                    // iban
-                    println(ibanMatch.value)
-                }
-            }
-        }
-    }
-}*/
 
 fun scrapeCompanyWallPage(urlToScrape: String): Company {
     val company: Company = skrape(BrowserFetcher) { // <--- pass BrowserFetcher to include rendered JS
