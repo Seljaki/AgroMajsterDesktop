e2611153ba2c77e22599eb4a053ad14e9c1f0997
mzHap <mojcazamuda12@gmail.com>
2024-06-08 21:24:19 +0200

    SCRUM-121 finished adding the pure basics

--- src/main/kotlin/ui/menu.kt
+++ src/main/kotlin/ui/menu.kt
@@ -6,12 +6,11 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.*
-import http.TOKEN
 import logOut
 
 @Composable
-fun menu(onLogOutClicked: () -> Unit, onListCompaniesClicked: () -> Unit, onGeneratorClicked: () -> Unit){
-    MenuButton(Icons.Default.Share,"Scraper", onClick = { println("Scraper was clicked")})
+fun menu(onLogOutClicked: () -> Unit, onListCompaniesClicked: () -> Unit, onGeneratorClicked: () -> Unit, onScraperClicked: () -> Unit){
+    MenuButton(Icons.Default.Share,"Scraper", onClick = onScraperClicked)
     MenuButton(Icons.Default.Edit,"Generator", onClick = onGeneratorClicked)
     MenuButton(Icons.Default.Info,"List Companies", onClick = onListCompaniesClicked)
     MenuButton(Icons.Default.Lock,"Log out", onClick = { logOut(); onLogOutClicked() })
